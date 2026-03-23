10bb05c50d8e5faf5b90bf46ff8162be9ed41dce
l <56237933+lcdunne@users.noreply.github.com>
2026-03-23 07:52:34 +0000

    Wire up directories and post-gen hook for large project choice

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,4 +1,29 @@
 import os
+import shutil
 
-if "{{ cookiecutter.database }}" == "none":
-    os.remove(os.path.join("app", "database.py"))
+project_size = "{{ cookiecutter.project_size }}"
+database = "{{ cookiecutter.database }}"
+
+if project_size == "small":
+    large_project_directories = [
+        "api",
+        "client",
+        "database",
+        "middleware",
+        "repository",
+        "schemas",
+        "services",
+        "utils",
+    ]
+
+    for directory in large_project_directories:
+        shutil.rmtree(os.path.join("app", directory))
+
+    if database == "none":
+        os.remove(os.path.join("app", "database.py"))
+else:
+    for module in ["database.py", "dependencies.py"]:
+        os.remove(os.path.join("app", module))
+
+    if database == "none":
+        shutil.rmtree(os.path.join("app", "database"))
